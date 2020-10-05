89646e410a3a1efe57db4c49461a59512ab1f151
Toni <toni@Jess-MacBook-Pro.local>
2020-10-05 00:51:37 -0400

    form updates/handlers

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Route, Link } from "react-router-dom"
 import Header from "./Header"
 import Form from "./Form"
 import Dashboard from "./Dashboard"
+
+
+
 function App() {
+  const [wordData, setWordData] = useState({});
+
+  const handleSubmit = async (word) => {
+    console.log('this is handleSubmit in app')
+  
+    let dictionaryUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=ded20abd-0943-4f6c-bc39-7a0b56aa660e`;
+
+    fetch(dictionaryUrl)
+			.then((res) => res.json())
+			.then((data) => setWordData(data));
+
+    // const res = await fetch(dictionaryUrl)
+    // const json = await res.json()
+    // setWordData(data)
+    
+  }
+
   return (
-    <div className="App">
-       <Header/>
-       <Form />
-       <Dashboard />
-    </div>
-  );
+		<div className='App'>
+			<nav className='Header'>
+				<Link to='/'>
+					<Header />
+				</Link>
+			</nav>
+			<div className='Search'>
+				<Form handleSubmit={handleSubmit}/>
+			</div>
+			<main className='Dashboard'>
+				<Dashboard />
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- project2/src/Form.js
+++ project2/src/Form.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React, {useState} from 'react'
+
 const Form =(props)=> {
+console.log('this is props - ', props)
+    const [definition, setDefinition] = useState ('')
+    const handleSubmit = e => {
+        console.log('handleSubmit clicked')
+        e.preventDefault()
+        props.handleSubmit(definition)
+        setDefinition('')
+    }
+    const handleChange = e => {
+        console.log('handleChange')
+        const word = e.target.value
+        setDefinition(word)
+    }
     return (
-		<form>
-			<input type='search'></input>
-			<button>search</button>
+		<form onSubmit={handleSubmit}>
+            <input 
+            type='text'
+            placeholder='Your Word'
+            value={definition}
+            onChange={handleChange}
+          />
+            <input 
+            type='submit'
+            value='Find Your Word'
+            />
 		</form>
 		);
 }
