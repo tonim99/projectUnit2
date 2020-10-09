90bc1725e33f64f954def4f2263e38f80cd76f36
Toni <toni@Jess-MacBook-Pro.local>
2020-10-09 02:01:23 -0400

    updates

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.scss';
-import { Route, Link, Switch } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import Header from "./Header"
 import Form from "./Form"
 import Main from './Main'
--- project2/src/WordInfo.js
+++ project2/src/WordInfo.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Link, Route} from "react-router-dom"
+import {Link} from "react-router-dom"
 const WordInfo = ({wordData}) => {
     return (
         <div className= 'wordList'>
