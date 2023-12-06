9d32f5cd6b1bcb88c5e0835b883e161601924a26
Tonimarie <morris.tonimarie@gmail.com>
2023-12-06 15:12:10 -0500

    Remove router from App

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,21 +1,20 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import './App.scss';
-import { Route, Switch } from "react-router-dom"
 import Header from "./Header"
 import Form from "./Form"
 import WordInfo from './WordInfo'
-import Word from './Word'
 import { fetchWord } from './api/api';
 
 function App() {
 	
-	const [wordData, setWordData] = useState()
-	const [word, setWord] = useState('')
-	const [input, setInput] = useState()
+	const [wordData, setWordData] = useState([])
+	const [word, setWord] = useState()
+	const [input, setInput] = useState('')
 	const [error, setError] = useState();
 
   	const getWord = useCallback(() => {
-		if(word){
+		
+		if(word !== undefined){
 			fetchWord(word)
 				.then((data) => {
 					setWordData(data)
@@ -29,7 +28,6 @@ function App() {
 	useEffect(() => {
 		getWord()
 	}, [getWord])
-
 	return (
 		<div className='app'>
 			<Header />
@@ -38,15 +36,7 @@ function App() {
 				setInput={setInput} 
 				setWord={setWord}
 			/>
-			<Switch>
-				<Route
-					path='/words/:definition'
-					render={(routerProps) => <Word {...routerProps} wordData={wordData}/>}
-				/>
-				<Route path='/'>
-					{wordData && <WordInfo wordData={wordData} />}
-				</Route>
-			</Switch>
+			<WordInfo wordData={wordData} word={word} error={error}/>
 			{error && <div>{error}</div>}
 		</div>
 	);
