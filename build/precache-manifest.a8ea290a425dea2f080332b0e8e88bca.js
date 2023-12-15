a5ed0190092b7e63777ce8590e39799b4ead62e6
Tonimarie <morris.tonimarie@gmail.com>
2023-12-15 11:10:59 -0500

    Remove unnecessary folder

---
+++ build/precache-manifest.a8ea290a425dea2f080332b0e8e88bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392a2de51ef07813c610128304c305db",
+    "url": "/ProjectUnit2/index.html"
+  },
+  {
+    "revision": "93ecd2268df3c8c14fb0",
+    "url": "/ProjectUnit2/static/css/main.71f02ba8.chunk.css"
+  },
+  {
+    "revision": "346ae7f694028d92ae35",
+    "url": "/ProjectUnit2/static/js/2.3c313ed9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ProjectUnit2/static/js/2.3c313ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ecd2268df3c8c14fb0",
+    "url": "/ProjectUnit2/static/js/main.1b73d357.chunk.js"
+  },
+  {
+    "revision": "5b3f39acee6d200be058",
+    "url": "/ProjectUnit2/static/js/runtime-main.cda0098d.js"
+  }
+]);
